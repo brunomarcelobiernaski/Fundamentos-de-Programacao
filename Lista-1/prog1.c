6424e67f547e7bdab6ecfc137eb1a47348b7d918
Bruno M Biernaski <brunobiernaski@alunos.utfpr.edu.br>
2022-05-02 20:54:00 -0300

    prog.1

---
+++ Lista-1/prog1.c
@@ -0,0 +1,21 @@
+/*Leia do teclado seis valores inteiros e em seguida mostre na tela os valores lidos na ordem inversa.*/
+
+#include <stdio.h>
+#define n 6
+
+int main()
+{
+    int vet[n], i;
+    
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &vet[i]);
+    }
+
+    for(i= n; i > 0; i--)
+    {
+        printf("%d ", vet[i-1]);
+    }
+
+    return 0;
+}
