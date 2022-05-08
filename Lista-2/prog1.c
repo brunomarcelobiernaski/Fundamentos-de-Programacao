90499ee5aee1ca8f11e9aea322e33965f48b9a92
brunomarcelobiernaski <brunobiernaski@alunos.utfpr.edu.br>
2022-05-08 15:43:15 -0300

    upload prog1 - L2

---
+++ Lista-2/prog1.c
@@ -0,0 +1,52 @@
+/*Desenvolva um programa para ler um caractere alfabético e a seguir mostre no dispositivo de saída os respectivos caracteres antecessor (quando não houver mostrar “*”) e sucessor (quando não houver mostrar “*”) do caractere lido.*/
+
+#include <stdio.h>
+
+int main()
+{
+    char b;
+
+    scanf("%c", &b);
+
+    if(b >= 65 && b <= 90)
+    {
+        if(b == 65 || b == 90)
+        {
+            if(b == 65)
+            {
+            printf("* {%c} %c\n", b, b + 1);
+            }
+            else
+            {
+                printf("%c {%c} *\n", b-1, b);
+            }
+        }
+        else
+        {
+            printf("%c {%c} %c\n", b-1, b ,b+1);
+        }
+    }
+
+    else if(b >= 97 && b <= 122)
+    {
+        if(b == 97 || b == 122)
+        {
+            if(b == 97)
+            {
+            printf("* {%c} %c\n", b, b + 1);
+            }
+            else
+            {
+                printf("%c {%c} *\n", b-1, b);
+            }
+        }
+        else
+        {
+            printf("%c {%c} %c\n", b-1, b ,b+1);
+        }
+
+    }
+
+
+    return 0;
+}
